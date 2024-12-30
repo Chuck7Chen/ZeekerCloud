5981eb75a9fe6b2d168b54eb0d8f9793ccbdc94d
chuck7chen <chuckcchen@gmail.com>
2024-12-30 16:34:48 +0800

    feat: image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 // Learn more on how to config.
 // - https://github.com/ant-tool/atool-build#配置扩展
 
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+
 module.exports = function(webpackConfig) {
   webpackConfig.babel.plugins.push('transform-runtime');
   webpackConfig.babel.plugins.push(['import', {
     libraryName: 'antd',
     style: 'css',
   }]);
+  webpackConfig.plugins.push(new ImageminWebpWebpackPlugin());
 
   return webpackConfig;
 };
